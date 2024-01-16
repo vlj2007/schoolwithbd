32d5a60e1d9b8901895344cb702dae0b46065da7
vlj2007 <vlj2007@yandex.ru>
2024-01-16 20:13:59 +0700

    init commit

---
+++ SchoolwithbdApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwartswithbd.schoolwithbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolwithbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolwithbdApplication.class, args);
+	}
+
+}
